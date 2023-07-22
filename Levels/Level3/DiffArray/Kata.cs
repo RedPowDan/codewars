5b1e086462969d41832a31b2d2c1adc30944623b
Danil Chernyh <dchernyh@syntellect.ru>
2023-07-22 13:54:46 +0500

    Level 3 array Diff

---
+++ Levels/Level3/DiffArray/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Levels.Level3.DiffArray
+{
+    public class Kata<T>
+    {
+        public static T[] ArrayDiff(T[] a, T[] b)
+        {
+            return a.Where(item => !b.Contains(item)).ToArray();
+        }
+    }
+}
---
+++ Tests/Level3/DiffArray/KataTests.cs
@@ -0,0 +1,26 @@
+﻿using Levels.Level1.DrawStairs;
+using Levels.Level3.DiffArray;
+using NUnit.Framework;
+using Tests.Helpers;
+
+namespace Tests.Level3.DiffArray
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void Test1()
+        {
+            HelperTests<bool>.Run(() =>
+            {
+                Assert.AreEqual(new[] {2},       Kata<int>.ArrayDiff(new[] {1, 2},    new[] {1}));
+                Assert.AreEqual(new[] {2, 2},    Kata<int>.ArrayDiff(new[] {1, 2, 2}, new[] {1}));
+                Assert.AreEqual(new[] {1},       Kata<int>.ArrayDiff(new[] {1, 2, 2}, new[] {2}));
+                Assert.AreEqual(new[] {1, 2, 2}, Kata<int>.ArrayDiff(new[] {1, 2, 2}, new int[] {}));
+                Assert.AreEqual(new int[] {},    Kata<int>.ArrayDiff(new int[] {},        new[] {1, 2}));
+                Assert.AreEqual(new[] {3}, Kata<int>.ArrayDiff(new[] {1, 2, 3}, new[] {1, 2}));
+                return true;
+            });
+        }
+    }
+}
